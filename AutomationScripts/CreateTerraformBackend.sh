48812aa2e7290c870f0466c44a84f930a0e72f2a
José Manuel Ordoñez Castillo <jmordonez@domcoin.net>
2019-04-20 17:59:56 -0600

    update script

--- AutomationScripts/CreateTerraformBackend.sh
+++ AutomationScripts/CreateTerraformBackend.sh
@@ -1,8 +1,8 @@
-#! bin/bash
+#!/usr/bin/env bash
 #This script will create: an Azure resource group, an Storage account and a Storage container which will be used to store terraform state
 
-az group create --locataion eastus2 --name $0
+az group create --location eastus2 --name $TERRAFORM_RG
 
-az storage account create --name $1 --resource-group $0 --location eastus2 --sku Standard_LRS
+az storage account create --name $TERRAFORM_SA --resource-group $TERRAFORM_RG --location eastus2 --sku Standard_LRS
 
-az storage container create --name terraform --account-name $1
+az storage container create --name terraform --account-name $TERRAFORM_SA
--- AutomationScripts/GetTerraformStorageKey.sh
+++ AutomationScripts/GetTerraformStorageKey.sh
@@ -1,3 +1,5 @@
-#! bin/bash
+#!/usr/bin/env bash
 
 # This script will fetch storage key which is required in terraform file to authenticate backend storage account
+
+az storage account keys list -g MyResourceGroup -n MyStorageAccount
